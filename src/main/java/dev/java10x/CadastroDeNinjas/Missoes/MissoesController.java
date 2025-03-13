4394971dae4cf9408e4c0bbec338f53f3197d884
lazaro <lazaroalves12355@gmail.com>
2025-03-13 15:54:03 -0300

    feat: Altera a rota do controller para listar as missoes e injeta dependencia do service e do repository

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,23 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    @GetMapping("/listarMissoes")
+    public List<MissoesModel> listarMissoes() {
+        return missoesService.listarMissoes();
+    }
 }
